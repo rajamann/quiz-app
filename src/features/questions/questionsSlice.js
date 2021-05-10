97252701055a2f64b2a6bafee23091aa54a07239
Raja <raja.mann@crunch.co.uk>
2021-05-10 22:43:32 +0100

    move questionsList to separate file

--- src/features/questions/questionsSlice.js
+++ src/features/questions/questionsSlice.js
@@ -1,80 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import questionsList from '../../config/questionsList.json'
+
 const initialState = {
-  questionsList: [
-    {
-      id: 1,
-      question: 'What is the capital of England?',
-      answers: [
-        {
-          answer: 'New York',
-          correct: false
-        },
-        {
-          answer: 'London',
-          correct: true
-        },
-        {
-          answer: 'Paris',
-          correct: false
-        },
-        {
-          answer: 'Amsterdam',
-          correct: false
-        }
-      ],
-      attempted: false,
-      correct: false
-    },
-    {
-      id: 2,
-      question: 'What is the capital of Canada?',
-      answers: [
-        {
-          answer: 'Vancouver',
-          correct: false
-        },
-        {
-          answer: 'Toronto',
-          correct: false
-        },
-        {
-          answer: 'Alberta',
-          correct: false
-        },
-        {
-          answer: 'Ottawa',
-          correct: true
-        }
-      ],
-      attempted: false,
-      correct: false
-    },
-    {
-      id: 3,
-      question: 'What is the capital of Australia?',
-      answers: [
-        {
-          answer: 'Sydney',
-          correct: false
-        },
-        {
-          answer: 'Brisbane',
-          correct: false
-        },
-        {
-          answer: 'Canberra',
-          correct: true
-        },
-        {
-          answer: 'Adelaide',
-          correct: false
-        }
-      ],
-      attempted: false,
-      correct: false
-    }
-  ],
+  questionsList,
   currentQuestion: 0,
   numberOfCorrectAnswers: 0,
   numberOfAttemptedQuestions: 0,
